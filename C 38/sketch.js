e6b3f7ad8e4ffd740ea4a1641c42b6b6d04963a6
pragya0310 <73340960+pragya0310@users.noreply.github.com>
2021-04-05 17:13:55 +0530

    Update sketch.js

--- C 38/sketch.js
+++ C 38/sketch.js
@@ -2,8 +2,18 @@ var db;
 var gamestate=0;
 var playercount=0;
 var player,form,game;
+var cs1,cs2,cs3,cs4,cars,c1,c2,c3,c4;
+var allPlayers;
+function preload(){
+ 
+    c1=loadImage("car1.png")
+    c2=loadImage("car2.png")
+    c3=loadImage("car3.png")
+    c4=loadImage("car4.png")
 
-
+    bg=loadImage("track.png")
+   
+}
 
 
 
@@ -14,11 +24,13 @@ db=firebase.database();
 game=new Game()
 game.getGs()
 game.start()
-
+cars=[]
 }
 function draw(){
-background("black")
-if(playercount==4){gamestate=1}
+background(255)
+if(playercount==4){gamestate=1;game.writeGs(1)}
+if(gamestate==1){game.play();drawSprites()}
 
-game.writeGs(1)
+
+
 }
